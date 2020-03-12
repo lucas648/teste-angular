1673036b1145e7fc14031df1972aae4873ab994f
Lucas Costa <lucascosta_s_a@hotmail.com>
2020-03-11 23:54:00 -0300

    ajustados metodos de requisição

--- src/app/services/cadastro.service.ts
+++ src/app/services/cadastro.service.ts
@@ -1,16 +1,21 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CadastroService {
   userUrl = 'http://api.template.megaleios.com/api/v1/Profile/Register';
+  httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type': 'application/json'
+    })
+  }
   constructor(private http: HttpClient) { }
 
   Criar (user) {
     this.http
-    .post(this.userUrl, JSON.stringify(user))
+    .post(this.userUrl, JSON.stringify(user), this.httpOptions)
     .subscribe()
   }
 }
--- src/app/services/login-service.service.ts
+++ src/app/services/login-service.service.ts
@@ -1,16 +1,21 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LoginServiceService {
   loginUrl = 'http://api.template.megaleios.com/api/v1/Profile/Token'
+  httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type': 'application/json'
+    })
+  }
   constructor(private http: HttpClient) { }
 
   Criar(user) {
     this.http
-    .post(this.loginUrl, JSON.stringify(user))
+    .post(this.loginUrl, JSON.stringify(user), this.httpOptions)
     .subscribe()
   }
 }
