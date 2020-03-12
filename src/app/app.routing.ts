1e78900ee696a9310d4cc676f053d14360b072b7
Lucas Costa <lucascosta_s_a@hotmail.com>
2020-03-11 21:42:53 -0300

    ajustes nas rotas

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,12 +1,12 @@
 import { Routes, RouterModule } from "@angular/router";
-import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { CadastroComponent } from './cadastro/cadastro.component';
 import { ListComponent } from './list/list.component';
 import { ModuleWithProviders } from '@angular/core';
+import { HomeComponent } from './home/home.component';
 
 const APP_route: Routes = [
-  {path: '', component: AppComponent},
+  {path: '', component: HomeComponent},
   {path: 'login', component: LoginComponent},
   {path: 'cadastro', component: CadastroComponent},
   {path: 'lista', component: ListComponent}
